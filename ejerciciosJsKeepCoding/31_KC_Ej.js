3da486a82e7fbf0f299838ea342e885e799dd852
anbreaker <antunez19@gmail.com>
2019-09-13 18:56:55 +0200

    Renombrados los Alumnos creados

--- ejerciciosJsKeepCoding/31_KC_Ej.js
+++ ejerciciosJsKeepCoding/31_KC_Ej.js
@@ -36,10 +36,10 @@ class Alumno (matricula,nombre,apellidos,email,estatusInscrito) {
 
 // matricula(Number),nombre(String),apellidos(String),email(String),estatusInscrito(boolean)
 
-let Sergio = new Persona(1,'Sergio','Tardio', 'sergio@gmail.com', true)
-let Pablo = new Persona(2,'Pablo', 'Escobar', 'gaviria@gmail.com', false)
-let Simao = new Persona(3,'Simao', 'Espor', 'simao@gmail.com', true)
-let Javier = new Persona(4,'Javier', 'Antunez', 'javier@gmail.com', false)
-let Jessica = new Persona (5,'Jessica', 'Neves', 'jessica@gmail.com', true)
+let Sergio = new Alumno(1,'Sergio','Tardio', 'sergio@gmail.com', true)
+let Pablo = new Alumno(2,'Pablo', 'Escobar', 'gaviria@gmail.com', false)
+let Simao = new Alumno(3,'Simao', 'Espor', 'simao@gmail.com', true)
+let Javier = new Alumno(4,'Javier', 'Antunez', 'javier@gmail.com', false)
+let Jessica = new Alumno (5,'Jessica', 'Neves', 'jessica@gmail.com', true)
 
 let infoPersonas = [Javier,Jessica,Sergio,Pablo,Simao]
