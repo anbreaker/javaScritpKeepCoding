3246b17487c5d108077d856e17f3a3d3026dd823
anbreaker <antunez19@gmail.com>
2019-09-14 09:26:02 +0200

    Ejemplo de Prototipos

---
+++ prototipo.js
@@ -0,0 +1,26 @@
+//Programacion funcional
+
+let array = [1,2,3,4];
+let array2 = [1,2,3,6];
+
+function sum(array){
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result;
+}
+
+//Programacion ECMA6 con prototype
+
+Array.prototype.sum = function sum () {
+    let result = 0;
+    for (let i = 0; i < this.length; i++) {
+        result += this[i];
+    }
+    return result;
+}
+
+console.log('Funcions: ',sum(array));    //Funcion 
+console.log('Prototype: ', array.sum());   //Prototype
+console.log('Prototype: ', array2.sum());   //Prototype
