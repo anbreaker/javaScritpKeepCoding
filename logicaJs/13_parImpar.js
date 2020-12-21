05d873fb7e9ad91dfcdd0b7f45416ad548200bfa
anbreaker <antunez19@gmail.com>
2020-12-21 14:28:59 +0100

    ejercicios practicar

--- logicaJs/13_parImpar.js
+++ logicaJs/13_parImpar.js
@@ -1,3 +1,16 @@
 'use strict';
 // 13) Programa una función que determine si un número es par o impar,
 // Ej, miFuncion(29) devolverá Impar.
+
+const parImpar = (num = undefined) => {
+  if (num === undefined || typeof num !== 'number')
+    return console.log('No entraste un Nº');
+
+  return num % 2 === 0 ? console.log(`${num} es Par`) : console.log(`${num} es Impar`);
+};
+
+parImpar(2);
+parImpar(3);
+parImpar(0);
+parImpar(19);
+parImpar('a');
--- logicaJs/14_FarenheitCelsius.js
+++ logicaJs/14_FarenheitCelsius.js
@@ -1,3 +1,22 @@
 'use strict';
 // 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa,
 // Ej, miFuncion(0,"C") devolverá 32°F.
+
+const convertirGrados = (grados = undefined, unidad = undefined) => {
+  if (grados === undefined || typeof grados !== 'number')
+    return console.log('No entraste un Nº');
+  if (unidad === undefined) return console.log('No entraste el tipo de temperatura');
+  if (typeof unidad !== 'string' || unidad.length !== 1 || !/(C|F)/.test(unidad))
+    return console.log('No entraste correctamente el tipo C o F');
+
+  if (unidad === 'C') {
+    return console.log(`${grados}ºC = ${Math.round(grados * (9 / 5) + 32)}ºF`);
+  } else if (unidad === 'F') {
+    return console.log(`${grados}ºF = ${Math.round((grados - 32) * (5 / 9))}ºC`);
+  }
+};
+
+convertirGrados(32, 'F');
+convertirGrados(100, 'F');
+convertirGrados(100, 'C');
+convertirGrados('a');
